e2785e44e62e745594f14e143ddefe0024d69de1
Saurabh <saurabh@carmatec.com>
2015-03-17 15:45:47 +0530

    sbs_ipn_listener.php  changed

--- app/webroot/sbs_ipn_listener.php
+++ app/webroot/sbs_ipn_listener.php
@@ -1,11 +1,10 @@
 <?php       
         // ini_set('log_errors', true);
-	  //   ini_set('error_log', '../tmp/logs/error_ipn.log');
-				
-		// instantiate the IpnListener class	
+	//    ini_set('error_log', '../tmp/logs/error_ipn.log');
+	// instantiate the IpnListener class	
 		include('IpnListener.php');	
 		$listener = new IpnListener();
-		$listener->use_sandbox = true;
+		$listener->use_sandbox = false;
 		
 		//To post over standard HTTP connection, use:
 		//$listener->use_ssl = false;
@@ -20,15 +19,13 @@
 		    error_log($e->getMessage());
 		    exit(0);
 		}
-		
 		$DB_HOST = 'localhost';
 		$DB_USER = 'cantorix_invoice';
-		$DB_PASS =  'CantorixInvoicingFrikkie5!',
+		$DB_PASS =  'CantorixInvoicingFrikkie5!';
 		$DB_NAME =  'cantorix_invoicing';	
 		$dbcon = mysql_connect($DB_HOST,$DB_USER,$DB_PASS) or die(mysql_error());		
 	    mysql_select_db($DB_NAME, $dbcon) or die(mysql_error());
-			
-		
+     		
 		/*
 		The processIpn() method returned true if the IPN was "VERIFIED" and false if it
 		was "INVALID".
@@ -67,7 +64,6 @@
 						$currency				=	$invoice_details['invoice_currency_code'];
 						$reciept_amount			=	$invoice_details['invoice_total'];
 						$reciept_amount			=	money_format('%!(.2n',$reciept_amount);
-							
 						if(($payment_status == 'Completed') && !($isTxnIdProcessed) && ($receiver_email == $businessEmail) && ($curncy == $currency) && ($paid_amount == $reciept_amount) ) {
 							
 							$status = 'Paid';
@@ -86,12 +82,19 @@
 																
 			}					    
 		    $clientDetails = getClientDetails($acr_client_id, $sbs_subscriber_id);
-		    $name 		   = $clientDetails['name'];
-			$sur_name	   = $clientDetails['sur_name'];
-			$customer_name = $name.' '.$sur_name;
-			$email 		   = $clientDetails['email'];
-			$msg = "Dear $customer_name, We have Recieved your payment, Thank you.";	
-		    mail("$email", 'Payment Recieved', $msg);
+		$msg                   = getPaymentReceivedTemplate($sbs_subscriber_id,'Payment',$payment_date,$paid_amount,$acr_client_invoice_id,$acr_client_id,$cpn_payment_method_id);
+                    $body_content =   $msg['content'];
+                    $from      = $msg['from'];
+                        $to        = $clientDetails['email'];
+                        $subject        = $msg['subject'];
+                        ob_start();
+                        $headers = "MIME-Version: 1.0" . "\r\n";
+                        $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+                $header .= "Content-Transfer-Encoding: 7bit\r\n\r\n";
+                $header .= $body_content."\r\n\r\n";
+                        $header = "From: "."<".$from.">\r\n";
+                $header .= "Reply-To: ".'admin@cantorix.com'."\r\n";
+                        $success = mail($to, $subject, $body_content, $header);
 			 		
 		
 		} else {
@@ -207,6 +210,77 @@
 			}	
 		}		
 				
+function getPaymentReceivedTemplate($subscriberId,$template,$payment_date,$payment_fee,$acr_client_invoice_id,$acr_client_id,$cpn_payment_method_id){
+                        global $dbcon;
+                        $qry = "select * from sbs_email_template_details where module_related = '$template' and sbs_subscriber_id = $subscriberId";
+                        $qry1 = "select * from acr_clients where id = $acr_client_id and sbs_subscriber_id = $subscriberId";
+                        $qry2 = "select * from acr_client_invoices where id = $acr_client_invoice_id and sbs_subscriber_id = $subscriberId";
+                        $qry3 = "select * from acr_client_contacts where acr_client_id = $acr_client_id and sbs_subscriber_id = $subscriberId";
+                        $result = mysql_query($qry, $dbcon);
+                        if($result){
+                                 while($row = mysql_fetch_array($result)) {
+                                         $subject       = $row['subject'];
+                                         $body                  = $row['body_content'];
+                                         $from                  = $row['from_email_address'];
+                                }
+                $result1 = mysql_query($qry1, $dbcon);
+                        if($result1){
+                                 while($row1 = mysql_fetch_array($result1)) {
+                                         $organisationName = $row1['organization_name'];
+                                         $website       =       $row1['website'];
+                                         $businessPhone = $row1['business_phone'];
+                                         $fax                   =       $row1['business_fax'];
+                                }
+                        }
+
+                        $result2 = mysql_query($qry2, $dbcon);
+                        if($result2){
+                                while($row2 = mysql_fetch_array($result2)) {
+                                         $invoiceNumber = $row2['invoice_number'];
+                                         $invoiceCurrencyCode = $row2['invoice_currency_code'];
+                                         $invoiceTotal = $row2['invoice_total'];
+                                         $invoiceDate   =       $row2['invoiced_date'];
+                                         $po    =       $row2['purchase_order_no'];
+                                         $invoiceDescription = $row2['description'];
+                                }
+                        }
+                        $result3 = mysql_query($qry3, $dbcon);
+			 if($result3){
+                                 while($row3 = mysql_fetch_array($result3)) {
+                                         $name = $row3['name'];
+                                         $surname = $row3['sur_name'];
+                                }
+                        }
+                        $paidAmount     = money_format('%!(.2n',$payment_fee);
+                        if($paidAmount){
+                                $balanceAmount = money_format('%!(.2n',$invoiceTotal) - $paidAmount;
+                        }else{
+                                $balanceAmount = money_format('%!(.2n',$invoiceTotal);
+                        }
+                                $swears = array(
+                                                "[Invoice No]"                          => $invoiceNumber,
+                                                "[Payment Date]"                        => $payment_date,
+                                                "[Receipt Amount]"              =>  $invoiceCurrencyCode.' '.money_format('%!(.2n',$paidAmount),
+                                                "[Balance]"                             => $invoiceCurrencyCode.' '.money_format('%!(.2n',$invoiceTotal),
+                                                "[Balance Due]"                         => $invoiceCurrencyCode.' '.money_format('%!(.2n',$balanceAmount),
+                                                "[Invoice Date]"                        => $invoiceDate,
+                                                "[PO Number]"                           => $po,
+                                                "[Invoice Description]"         => $invoiceDescription,
+                                                "[Organization Name]"           => $organisationName,
+                                                "[Organization Website]"        => $website,
+                                                "[Business Phone]"              => $businessPhone,
+                                                "[Business Fax]"                        => $fax,
+                                                "[Primary Contact Name]"        => $name,
+                                                "[Primary Contact Surname]"     => $surname
+                                );
+                                $msg['subject'] = str_replace(array_keys($swears), array_values($swears), $subject);
+                                $msg['content'] = str_replace(array_keys($swears), array_values($swears), $body);
+                                $msg['from']    =       $from;
+                                return $msg;
+                        }else{
+                                return false;
+                        }
+                }
 		
 	
 ?>
